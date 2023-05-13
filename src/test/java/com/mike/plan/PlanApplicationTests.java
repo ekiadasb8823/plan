92076f14c2bd75407236b2250f145f28bd7f9df8
ekiadasb <827773174@qq.com>
2023-05-13 11:12:48 +0000

    初始化项目

---
+++ src/test/java/com/mike/plan/PlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mike.plan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
